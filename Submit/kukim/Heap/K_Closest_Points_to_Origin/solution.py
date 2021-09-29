0497408a87b12a774e7d5aefe38c9bcb8f32198a
“kukim” <kunheeya@gmail.com>
2021-09-29 19:40:45 +0900

    Heap-kukim

--- Submit/kukim/Heap/K_Closest_Points_to_Origin/solution.py
+++ Submit/kukim/Heap/K_Closest_Points_to_Origin/solution.py
@@ -1,6 +1,18 @@
 from typing import List
+import heapq
+
+# heap
+def kClosest(points: List[List[int]], k: int) -> List[List[int]]:
+    heap = []
+    for x, y in points:
+        heapq.heappush(heap, ((x ** 2 + y ** 2), [x,y]))
+    
+    return [heapq.heappop(heap)[1] for _ in range(k)]
+
+# list sorting
 def kClosest(points: List[List[int]], k: int) -> List[List[int]]:
     return sorted(points, key = lambda x : x[0] ** 2 + x[1] ** 2)[:k]
+
 
 points = [[3,3],[5,-1],[-2,4]]
 k = 2
